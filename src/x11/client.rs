b1c7104155b0dd83485be104d2eab7d7019fc4b3
Yash Karandikar <nerdstep710@gmail.com>
2022-06-27 13:13:22 +0530

    Add license text to source files

--- src/x11/client.rs
+++ src/x11/client.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2022 Infoshock Tech
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use breadx::prelude::{AsyncDisplayXprotoExt, SetMode};
 use breadx::{AsyncDisplay, BreadError, ConfigureWindowParameters, ErrorCode, EventMask, Window};
 use slotmap::{new_key_type, SlotMap};
@@ -164,7 +179,10 @@ impl XcrabWindowManager {
                     };
 
                     let new_pane_key = if child_key == parent_key {
-                        self.rects.insert_with_key(|key| Rectangle { parent: key, ..new_pane })
+                        self.rects.insert_with_key(|key| Rectangle {
+                            parent: key,
+                            ..new_pane
+                        })
                     } else {
                         self.rects.insert(new_pane)
                     };
